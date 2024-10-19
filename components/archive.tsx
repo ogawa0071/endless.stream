0ef6955c50d8810b758403e243c31038059a8883
Futa Ogawa <ogawa0071@gmail.com>
2024-10-20 06:37:03 +0900

    コメント付きアーカイブを追加

--- components/archive.tsx
+++ components/archive.tsx
@@ -37,7 +37,52 @@ function VideoCard() {
               <div className="flex items-start justify-between">
                 <div>
                   <h2 className="text-lg font-semibold line-clamp-2">
-                    ぬきたし5日目
+                    ぬきたし5日目（コメント付き）
+                  </h2>
+                  <div className="flex items-center mt-1 space-x-2">
+                    <Avatar className="w-4 h-4">
+                      <AvatarImage src="/oechan.png" alt="oechan" />
+                    </Avatar>
+                    <div>
+                      <div className="flex items-center space-x-1">
+                        <span className="text-sm">oechan</span>
+                        <BadgeCheck
+                          className="w-4 h-4"
+                          fill="#9146ff"
+                          color="#ffffff"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <p className="text-sm text-muted-foreground mt-1">
+                    抜きゲーみたいな島に住んでる貧乳（わたし）はどうすりゃいいですか？
+                  </p>
+                </div>
+              </div>
+            </div>
+          </CardContent>
+        </Link>
+      </Card>
+
+      <Card className="w-full max-w-3xl mx-auto">
+        <Link href={"/nukitashi-5-without-comment"}>
+          <CardContent className="p-0">
+            <div className="relative aspect-video bg-black">
+              <div className="absolute inset-0 flex items-center justify-center">
+                <Image src={nukitashi4} alt="" />
+              </div>
+              <div className="absolute top-2 left-2 bg-black bg-opacity-60 text-white px-2 py-1 rounded text-sm">
+                7:12:39
+              </div>
+              <div className="absolute bottom-2 right-2 bg-black bg-opacity-60 text-white px-2 py-1 rounded text-sm">
+                2024/10/19 19:25
+              </div>
+            </div>
+            <div className="p-4">
+              <div className="flex items-start justify-between">
+                <div>
+                  <h2 className="text-lg font-semibold line-clamp-2">
+                    ぬきたし5日目（コメントなし）
                   </h2>
                   <div className="flex items-center mt-1 space-x-2">
                     <Avatar className="w-4 h-4">
@@ -82,7 +127,7 @@ function VideoCard() {
               <div className="flex items-start justify-between">
                 <div>
                   <h2 className="text-lg font-semibold line-clamp-2">
-                    ぬきたし4日目コメ付き
+                    ぬきたし4日目（コメント付き）
                   </h2>
                   <div className="flex items-center mt-1 space-x-2">
                     <Avatar className="w-4 h-4">
