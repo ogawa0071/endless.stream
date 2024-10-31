1f421ffbe4e1eb7d238c93d0b0fab5c729cf0d81
Futa Ogawa <ogawa0071@gmail.com>
2024-10-31 22:49:22 +0900

    広告のテスト

--- components/ad.tsx
+++ components/ad.tsx
@@ -1,39 +1,31 @@
 "use client";
 
-import { useIsClient } from "@uidotdev/usehooks";
+import Script from "next/script";
+import { useEffect } from "react";
 
 export function Ad() {
-  const isClient = useIsClient();
+  useEffect(() => {
+    window.admaxads = [
+      ...(window.admaxads || []),
+      {
+        admax_id: "3de8ed6819617e5c823eeb1924617298",
+        type: "switch",
+      },
+    ];
+
+    return () => {
+      window.admaxads = [];
+    };
+  }, []);
 
   return (
-    isClient && (
-      <>
-        <div
-          className="admax-ads"
-          data-admax-id="2d62901de614aa7f89dafac02fbe6cc5"
-          style={{ display: "inline-block", width: 728, height: 90 }}
-        />
-        {/* <Script
-        src="https://adm.shinobi.jp/st/t.js"
-        onReady={() => {
-          (window.admaxads = window.admaxads || []).push({
-            admax_id: "2d62901de614aa7f89dafac02fbe6cc5",
-            type: "banner",
-          });
-        }}
-      ></Script> */}
-        <script
-          type="text/javascript"
-          dangerouslySetInnerHTML={{
-            __html: `(admaxads = window.admaxads || []).push({admax_id: "2d62901de614aa7f89dafac02fbe6cc5",type: "banner"});`,
-          }}
-        ></script>
-        <script
-          type="text/javascript"
-          src="https://adm.shinobi.jp/st/t.js"
-          async
-        ></script>
-      </>
-    )
+    <>
+      <div
+        className="admax-switch"
+        data-admax-id="3de8ed6819617e5c823eeb1924617298"
+        style={{ display: "inline-block" }}
+      />
+      <Script src="https://adm.shinobi.jp/st/t.js"></Script>
+    </>
   );
 }
