4ffce0ff76c081708f0887f58c7e063b74a341c8
Futa Ogawa <ogawa0071@gmail.com>
2024-11-01 22:40:13 +0900

    広告をアップデート

--- components/ad.tsx
+++ components/ad.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useIsClient } from "@uidotdev/usehooks";
-import Script from "next/script";
 import { useEffect } from "react";
 
 export function Ad() {
@@ -23,26 +22,36 @@ export function Ad() {
 
   return (
     isClient && (
-      <div className="md:px-4">
+      <div className="md:px-4 grid gap-2">
         <div className="p-4 md:px-0 md:py-4">
           テスト的に広告を表示しています。数日で削除する予定です。
         </div>
-        <div
-          className="admax-switch"
-          data-admax-id="3de8ed6819617e5c823eeb1924617298"
-          style={{ display: "inline-block" }}
-        />
-        <Script src="https://adm.shinobi.jp/st/t.js"></Script>
         <div>
-          <ins className="widget-banner"></ins>
-          {/* eslint-disable-next-line */}
-          <script
-            async
-            className="widget-banner-script"
-            src="https://widget-view.dmm.co.jp/js/banner_placement.js?affiliate_id=ogawa0071-001&banner_id=227_728_90"
-          ></script>
+          <div
+            className="admax-switch"
+            data-admax-id="3de8ed6819617e5c823eeb1924617298"
+            style={{ display: "inline-block" }}
+          />
+          <script async src="https://adm.shinobi.jp/st/t.js"></script>
         </div>
+        <AdDMM bannerId="227" />
+        <AdDMM bannerId="644" />
       </div>
     )
+  );
+}
+
+function AdDMM({ bannerId }: { bannerId: string }) {
+  return (
+    <div>
+      <iframe
+        srcDoc={`<head><script src="https://cdn.tailwindcss.com"></script></head><body><ins class="widget-banner"></ins><script class="widget-banner-script" src="https://widget-view.dmm.co.jp/js/banner_placement.js?affiliate_id=ogawa0071-001&banner_id=${bannerId}_336_280"></script></body>`}
+        className="md:hidden w-[336px] h-[280px]"
+      ></iframe>
+      <iframe
+        srcDoc={`<head><script src="https://cdn.tailwindcss.com"></script></head><body><ins class="widget-banner"></ins><script class="widget-banner-script" src="https://widget-view.dmm.co.jp/js/banner_placement.js?affiliate_id=ogawa0071-001&banner_id=${bannerId}_728_90"></script></body>`}
+        className="hidden md:block w-[728px] h-[90px]"
+      ></iframe>
+    </div>
   );
 }
